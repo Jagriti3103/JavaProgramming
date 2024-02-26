ba8599b6a8066003d8290338605538cc7d64c364
Jagriti Gupta <jgupta.31103@gmail.com>
2024-02-26 21:14:25 +0530

    Lets Start Java Programming

---
+++ JavaBasics.java
@@ -0,0 +1,14 @@
+public class JavaBasics {
+    public static void main(String arg[]){
+        System.out.println("Hlelo World!");
+        //types of Datatypes in JAVA:
+        // 1.int (4 bytes)
+        // 2.float(4 bytes)
+        // 3.byte(1 byte)
+        // 4.short (2 bytes)
+        // 5. char (2 bytes)
+        // 6. double (8 bytes)
+        // 7. long (8 bytes)
+    }
+
+}
